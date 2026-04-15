06e4943d2d106073596fd611cd1266a3627dd1da
Priya Trivedi <priya100trivedi@gmail.com>
2026-04-15 09:21:54 +0000

    script read & echo added

---
+++ scripting/script.sh
@@ -0,0 +1,14 @@
+a=Priya  
+b=$(hostname) 
+
+#here we calling a command as variable
+
+echo "my name is $a"
+
+echo "your hostname $b"
+
+echo "tell me your ip"
+
+read c #here we calling variable dynamically
+
+echo "this is my ip $c"
